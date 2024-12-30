d5eaea3947ef2b14c8971a611f2cd4c797359a2a
Piotr Stec <piotr.stec2g@gmail.com>
2024-12-30 13:06:53 +0100

    deploy invalid class hash

---
+++ openrpc-testgen/src/suite_openrpc/suite_deploy/test_deploy_txn_v3_invalid_class_hash.rs
@@ -0,0 +1,43 @@
+use crate::{
+    assert_matches_result,
+    utils::v7::{
+        accounts::account::AccountError,
+        contract::factory::ContractFactory,
+        endpoints::errors::OpenRpcTestGenError,
+        providers::{jsonrpc::StarknetError, provider::ProviderError},
+    },
+    RandomizableAccountsTrait, RunnableTrait,
+};
+use rand::{rngs::StdRng, RngCore, SeedableRng};
+use starknet_types_core::felt::Felt;
+
+#[derive(Clone, Debug)]
+pub struct TestCase {}
+
+impl RunnableTrait for TestCase {
+    type Input = super::TestSuiteDeploy;
+
+    async fn run(test_input: &Self::Input) -> Result<Self, OpenRpcTestGenError> {
+        let factory = ContractFactory::new(
+            Felt::from_hex_unchecked("0x1234"),
+            test_input.random_paymaster_account.random_accounts()?,
+        );
+        let mut salt_buffer = [0u8; 32];
+        let mut rng = StdRng::from_entropy();
+        rng.fill_bytes(&mut salt_buffer[1..]);
+
+        let invoke_result = factory
+            .deploy_v3(vec![], Felt::from_bytes_be(&salt_buffer), true)
+            .send()
+            .await;
+
+        assert_matches_result!(
+            invoke_result.unwrap_err(),
+            AccountError::Provider(ProviderError::StarknetError(
+                StarknetError::TransactionExecutionError(_)
+            ))
+        );
+
+        Ok(Self {})
+    }
+}
